2bded610cdb7b21fb798ea01bd7e49f567c8884d
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-04-26 17:04:34 -0300

    corrige erros do lint

--- src/components/ProductsSearch.jsx
+++ src/components/ProductsSearch.jsx
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
- class ProductsSearch extends Component {
+class ProductsSearch extends Component {
   render() {
     return (
       <div className="search page">
-        <label htmlFor="">
-          <input type="text"></input>
+        <label htmlFor="product-input">
+          <input
+            type="text"
+            id="product-input"
+          />
         </label>
-        <h2 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+        <h2
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
       </div>
-    )
+    );
   }
 }
 
