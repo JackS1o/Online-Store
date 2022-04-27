4833c721945cafa253ae5cc75df70c6b83a5d06e
Guilherme Eduardo T Carvalho <91907319+guiduzera@users.noreply.github.com>
2022-04-27 18:59:21 -0300

    enviando requisito 7

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import ProductsSearch from './pages/ProductsSearch';
 import ShoppingCart from './pages/ShoopingCart';
 import Checkout from './pages/Checkout';
+import Details from './pages/Details';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={ ProductsSearch } />
         <Route exact path="/ShoppingCart" component={ ShoppingCart } />
         <Route exact path="/Checkout" component={ Checkout } />
+        <Route path="/details/:id" component={ Details } />
       </Switch>
     </BrowserRouter>
   );
