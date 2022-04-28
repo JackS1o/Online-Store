6daba1154e4dfcee7ac66fc13d2575eedb3d885a
Jackson <jviana116@gmail.com>
2022-04-28 20:41:13 -0300

    metade da 10

--- src/pages/ShoopingCart.jsx
+++ src/pages/ShoopingCart.jsx
@@ -4,8 +4,31 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      d: '',
+    };
+  }
+
+  componentDidMount() {
+    this.func();
+  }
+
+  func = (x) => {
+    const { productList } = this.props;
+    const j = productList.filter((item) => item === x);
+    console.log(j.length);
+    this.setState({ d: j.length });
+  }
+
   render() {
     const { productList } = this.props;
+    const { d } = this.state;
+    // https://pt.stackoverflow.com/questions/16483/remover-elementos-repetido-dentro-de-um-array-em-javascript#:~:text=O%20que%20este%20c%C3%B3digo%20faz,indice%20que%20a%20fun%C3%A7%C3%A3o%20passa.
+
+    const a = productList.filter((este, i) => productList.indexOf(este) === i);
+    console.log(d);
     return (
       <div>
         <Link
@@ -16,20 +39,28 @@ class ShoppingCart extends Component {
         </Link>
         {productList.length === 0
         && <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio.</h1>}
-        <p data-testid="shopping-cart-product-quantity">{productList.length}</p>
+        <p data-testid="shopping-cart-product-quantity">{a.length}</p>
         <div className="div-mae-card-carrinho">
-          {productList.map((elem) => (
+          {a.map((elem) => (
             <div
               key={ elem.title }
+              data-testid="shopping-cart-product-name"
               className="card-carrinho"
             >
               <p data-testid="shopping-cart-product-name">{ elem.title }</p>
               <img src={ elem.thumbnail } alt="Imagem" className="img-card" />
               <p>{ elem.price }</p>
-              <button data-testid="shopping-cart-button" type="button">adicionar</button>
+              <button
+                onClick={ () => this.func(elem) }
+                type="button"
+              />
+              <p>
+                  {d}
+                </p>
             </div>
           ))}
         </div>
+        <button data-testid="shopping-cart-button" type="button">adicionar</button>
       </div>
     );
   }
